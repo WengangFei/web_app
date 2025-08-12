3a2e4f44e65748b266a9f5bcf2e7a3a1763a5462
Fei <wengangfei@gmail.com>
2025-08-12 16:13:23 -0500

    seed database

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from '@/app/ui/fonts';
 
+//add meta data
+export const metadata = {
+  title: 'Pick-up',
+  description: 'Pick-up',
+  keywords: 'Pick-up',
+  icons: {
+    icon: '/opengraph-image.png',
+  }
+}
 export default function RootLayout({
   children,
 }: { 
